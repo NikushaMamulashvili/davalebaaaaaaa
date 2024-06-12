bee319739100cfb88c9e755798401f631c0d1d70
Nika Mamulashvili <164235570+NikushaMamulashvili@users.noreply.github.com>
2024-06-12 19:51:04 +0400

    Add files via upload

---
+++ JS2/script.js
@@ -0,0 +1,41 @@
+let numebi = 777
+switch(numebi) {
+    case 10:
+        console.log('cvladi aris 10')
+        break;
+        case 11:
+            console.log('cvladi aris 11')
+            break;
+            case 12:
+                console.log('cvladi aris 12')
+                break;
+                case 13:
+                    console.log('cvladi aris 13')
+                    break;
+                    case 14:
+                        console.log('cvladi aris 14')
+                        break;
+                            default:
+                                console.log('cvladi aris 15')
+}
+let names = ["nika tazo gio saba oto"]
+
+names.shift()
+
+names.push ("nika")
+
+if (names [4] == "tazo") {
+    console.log( "tazo")
+}
+else if (names [4] == "saba") {
+    console.log ("saba")
+}
+else if (names [4] == "nika") {
+    console.log ("nika")
+}
+else if (names [4] == "oto") {
+    console.log ("oto")
+}
+else {
+    console.log ("gio")
+}
